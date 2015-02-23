1e810988e70cf2bf928e0b0bd618109eff87e247
Humberto <humberto2do@gmail.com>
2015-02-22 23:53:16 -0600

    coding al route se agrega un controlador

--- app/js/router.js
+++ app/js/router.js
@@ -7,5 +7,9 @@ Test.Router.map(function() {
 Test.TestVocabularyRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('vocabulary',1);
+  },
+	setupController: function(controller, model) {
+		this._super(controller, model);
+    controller.set('model', model);
   }
 });
