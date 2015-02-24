80cecfe7a0f592fd5c81013d063d8abadfa3c3c5
Humberto <humberto2do@gmail.com>
2015-02-24 01:27:42 -0600

    coding se quita mayuscula para mejor manejo en el action del controller

--- app/js/model.js
+++ app/js/model.js
@@ -1,38 +1,38 @@
 
 Test.Vocabulary = DS.Model.extend({
-  spaceOne: DS.attr('string'),
-  spaceTwo: DS.attr('string'),
-  spaceThree: DS.attr('string'),
-  spaceFour: DS.attr('string'),
-  spaceFive: DS.attr('string'),
-  spaceSix: DS.attr('string'),
-  spaceSeven: DS.attr('string'),
-  spaceEight: DS.attr('string')
+  spaceone: DS.attr('string'),
+  spacetwo: DS.attr('string'),
+  spacethree: DS.attr('string'),
+  spacefour: DS.attr('string'),
+  spacefive: DS.attr('string'),
+  spacesix: DS.attr('string'),
+  spaceseven: DS.attr('string'),
+  spaceeight: DS.attr('string')
 });
 
 Test.Vocabulary.reopenClass({
   FIXTURES: [
  {
   id: 1,
-  spaceOne: 'in',
-  spaceTwo: 'gifted',
-  spaceThree: 'hopeless',
-  spaceFour: 'skilful',
-  spaceFive: 'of talented',
-  spaceSix: 'aptitude for',
-  spaceSeven: 'expert',
-  spaceEight: 'talented'
+  spaceone: 'in',
+  spacetwo: 'gifted',
+  spacethree: 'hopeless',
+  spacefour: 'skilful',
+  spacefive: 'of talented',
+  spacesix: 'aptitude for',
+  spaceseven: 'expert',
+  spaceeight: 'talented'
  },
  {
   id: 2,
-  spaceOne: 'Lorem Ipsum One',
-  spaceTwo: 'Lorem Ipsum Two',
-  spaceThree: 'Lorem Ipsum Three',
-  spaceFour: 'Lorem Ipsum Four',
-  spaceFive: 'Lorem Ipsum Five',
-  spaceSix: 'Lorem Ipsum Six',
-  spaceSeven: 'Lorem Ipsum Seven',
-  spaceEight: 'Lorem Ipsum Eight'
+  spaceone: 'Lorem Ipsum One',
+  spacetwo: 'Lorem Ipsum Two',
+  spacethree: 'Lorem Ipsum Three',
+  spacefour: 'Lorem Ipsum Four',
+  spacefive: 'Lorem Ipsum Five',
+  spacesix: 'Lorem Ipsum Six',
+  spaceseven: 'Lorem Ipsum Seven',
+  spaceeight: 'Lorem Ipsum Eight'
  }
   ]
 });
